d878420166a367ebc53e05411d3acc3b4dc0408b
Kenneth Chen <efeichen@gmail.com>
2019-04-27 22:35:40 -0700

    Add type.js

---
+++ main.js
@@ -0,0 +1,13 @@
+(() => {
+    // Typed.js configs
+    const typed = new Typed('#typed', {
+        stringsElement: '#typed-strings',
+        showCursor: false,
+        typeSpeed: 50,
+        backDelay: 2000,
+        backSpeed: 20,
+        loop: true,
+        loopCount: Infinity,
+        smartBackspace: true
+    });
+})();
